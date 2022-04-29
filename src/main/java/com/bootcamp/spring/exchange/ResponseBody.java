34ee512d27b7ae19b3a11eed306cbd7ce9a5ac7c
vishal bihani <vishal@coffeebeans.io>
2022-04-29 14:41:59 +0530

    [INCR-4]: Added response body class

---
+++ src/main/java/com/bootcamp/spring/exchange/ResponseBody.java
@@ -0,0 +1,31 @@
+package com.bootcamp.spring.exchange;
+
+public class ResponseBody {
+
+    private int statusCode;
+
+    private String message;
+
+    public ResponseBody() {}
+
+    public ResponseBody(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
