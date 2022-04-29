28cdd96263d778ec2ef9dd1cc601cb9d2bc309b1
vishal bihani <vishal@coffeebeans.io>
2022-04-29 12:03:03 +0530

    Initial commit

---
+++ src/main/java/com/bootcamp/spring/BootcampSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSpringApplication.class, args);
+	}
+
+}
