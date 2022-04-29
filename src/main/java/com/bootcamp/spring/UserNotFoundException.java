04f3bd18336e7c480c5dc9a07d40b81561ab4966
vishal bihani <vishal@coffeebeans.io>
2022-04-29 15:06:08 +0530

    [INCR-5]: Added UserNotFoundException

---
+++ src/main/java/com/bootcamp/spring/UserNotFoundException.java
@@ -0,0 +1,20 @@
+package com.bootcamp.spring;
+
+public class UserNotFoundException extends Exception {
+
+    private final String message;
+
+    public UserNotFoundException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    public UserNotFoundException(String message, Throwable t) {
+        super(message, t);
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
