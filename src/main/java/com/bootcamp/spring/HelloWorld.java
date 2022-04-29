eaae582038df5b61cba398932940fc42f4d032b1
vishal bihani <vishal@coffeebeans.io>
2022-04-29 12:39:19 +0530

    [INCR-2]: Added post method without request body

--- src/main/java/com/bootcamp/spring/HelloWorld.java
+++ src/main/java/com/bootcamp/spring/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.bootcamp.spring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,15 +14,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
     private static final String HELLO_WORLD_MSG = "Hello World!";
+    private static final String HELLO_MSG_FROM_POST = "Hello! You performed a post request successfully.";
+
 
     /**
      * This method will return the {@link #HELLO_WORLD_MSG} if the endpoint
      * <code>/hello</code> is requested.
      *
-     * @return
+     * @return String response
      */
     @GetMapping("/hello")
     public String returnHelloWorld() {
         return HELLO_WORLD_MSG;
+    }
+
+    /**
+     * This method will return the {@link #HELLO_MSG_FROM_POST} if the endpoint
+     * <code>/hello</code> is hit with HTTP POST request.
+     *
+     * @return String response
+     */
+    @PostMapping("/hello")
+    public String sayHelloFromPost() {
+        return HELLO_MSG_FROM_POST;
     }
 }
