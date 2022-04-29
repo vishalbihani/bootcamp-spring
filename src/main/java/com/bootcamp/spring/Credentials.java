e4dcaa1ff79004714c63ea15fc0193872d7ed542
vishal bihani <vishal@coffeebeans.io>
2022-04-29 13:07:26 +0530

    [INCR-2]: Added jackson json property

--- src/main/java/com/bootcamp/spring/Credentials.java
+++ src/main/java/com/bootcamp/spring/Credentials.java
@@ -1,9 +1,18 @@
 package com.bootcamp.spring;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Credentials {
 
+    /**
+     * If the name of the variable and the json property is not same
+     * then use <code>@JsonProperty(value)</code> to map two different values.
+     */
+
+    @JsonProperty("email")
     private String username;
 
+    @JsonProperty("password")
     private String password;
 
     public Credentials() {}
