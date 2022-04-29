643e8b4c5a9c8ac2ddd4d26a4c308a9811e1a968
vishal bihani <vishal@coffeebeans.io>
2022-04-29 15:59:18 +0530

    [INCR-6]: Added Input controller for BMI calculation

---
+++ src/main/java/com/bootcamp/spring/calculator/controller/InputController.java
@@ -0,0 +1,29 @@
+package com.bootcamp.spring.calculator.controller;
+
+import com.bootcamp.spring.basics.exchange.ResponseBody;
+import com.bootcamp.spring.calculator.BMICalculator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InputController {
+    private static final String BMI_RESULT_MSG = "Your BMI is: ";
+
+    @Autowired
+    BMICalculator bmiCalculator;
+
+    @GetMapping("/calculator/bmi")
+    public ResponseEntity<ResponseBody> calculateBMI(@RequestParam(name = "height") float height,
+                                                     @RequestParam(name = "weight") float weight) {
+
+        float bmi = bmiCalculator.calculateBMI(height, weight);
+        return new ResponseEntity<>(
+                new ResponseBody(HttpStatus.OK.value(), BMI_RESULT_MSG + bmi),
+                HttpStatus.OK
+        );
+    }
+}
